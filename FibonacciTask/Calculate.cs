4e414ae27873a36a4c7fd1590f742e44f6b059f7
PavelKhlebinski <predramer@gamil.com>
2021-04-14 18:46:10 +0300

    Add project files.

---
+++ FibonacciTask/Calculate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FibonacciTask
+{
+    public class Calculate
+    {
+        private static int Fib(int i)
+        {
+            if (i <= 1)
+            {
+                return i;
+            }
+            else
+            {
+                return Fib(i - 1) + Fib(i - 2);
+            }
+        }
+
+        public static async void FibAsync(int n)
+        {
+            Console.WriteLine($"Fibonacci number {n} is {await Task.Run(() => Fib(n))}");
+        }
+    }
+}
---
+++ FibonacciTask/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FibonacciTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate.FibAsync(5);
+            Calculate.FibAsync(7);
+            Calculate.FibAsync(9);
+            Console.ReadKey();
+        }
+    }
+}
